3279b48a57079fb085b05a6f213bfaa7ed6f24f6
Zhihao Tan <zhihao.tan@aquasec.com>
2025-03-22 16:21:11 +0000

    Add new file

---
+++ mongo-backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TIMESTAMP=\$(date +%Y%m%d_%H%M%S)
+BACKUP_NAME="mongodb_backup_\$TIMESTAMP"
+BUCKET_NAME="EKS-DB-Stack-public-mongodb-backups"
+
+# Perform MongoDB backup
+mongodump --archive=\$BACKUP_NAME.gz --gzip
+
+# Upload to S3
+aws s3 cp \$BACKUP_NAME.gz s3://\$BUCKET_NAME/
+
+# Clean up local backup
+rm \$BACKUP_NAME.gz
